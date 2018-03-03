1732791b6ff8a95d1084b0f81f82e79512030c6e
Tucui Mihai <mihaitucui2009@yahoo.com>
2018-03-03 21:49:12 +0200

    commit 03 martie

--- app/src/main/java/comcoffeesoftware/httpsgithub/inventarsoft/ContactActivity.java
+++ app/src/main/java/comcoffeesoftware/httpsgithub/inventarsoft/ContactActivity.java
@@ -1,11 +1,14 @@
 package comcoffeesoftware.httpsgithub.inventarsoft;
 
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -44,8 +47,32 @@ public class ContactActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        final ImageView facebookImageView = (ImageView) findViewById(R.id.Adresa_Facebook);
+        facebookImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setPackage("com.facebook.orca");
+                intent.setData(Uri.parse("https://m.me/"+"alx.dacian"));
+                startActivity(intent);
+            }
+        });
+        final ImageView whatsappImageView = (ImageView) findViewById(R.id.Whats_Up);
+        whatsappImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toNumber = "+40 78778 2071"; // contains spaces.
+                toNumber = toNumber.replace("+", "").replace(" ", "");
 
-
-
+                Intent sendIntent = new Intent("android.intent.action.MAIN");
+                sendIntent.putExtra("jid", toNumber + "@s.whatsapp.net");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, " ");
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setPackage("com.whatsapp");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
     }
 }
